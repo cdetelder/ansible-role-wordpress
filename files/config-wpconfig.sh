b1f9e77fe5ea63e7ab9b116032c6adcb670a1a55
cdetelder <camille.detelder03@gmail.com>
2025-06-27 11:27:55 +0000

    Add configure apache2 (only Debian)

--- files/config-wpconfig.sh
+++ files/config-wpconfig.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+cd /var/www/html/wordpress/
+
 sed -i "s/database_name_here/wordpress/" wp-config.php 
 sed -i "s/username_here/example/" wp-config.php 
 sed -i "s/password_here/examplePW/" wp-config.php 
