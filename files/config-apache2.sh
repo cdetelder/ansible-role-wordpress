9562f37f42a3d5a780d5b104048af7dea64eb31e
cdetelder <camille.detelder03@gmail.com>
2025-06-27 12:12:43 +0000

    Fix apache2 permissions issues + wordpress working

--- files/config-apache2.sh
+++ files/config-apache2.sh
@@ -1,13 +1,9 @@
-#!/bin/bash
-
-cat > /etc/apache2/sites-available/wordpress.conf <<EOF 
 <VirtualHost *:80> 
     ServerAdmin admin@localhost 
     DocumentRoot /var/www/html/wordpress 
     <Directory /var/www/html/wordpress> 
         AllowOverride All 
     </Directory> 
-    ErrorLog \${APACHE_LOG_DIR}/error.log 
-    CustomLog \${APACHE_LOG_DIR}/access.log combined 
+    ErrorLog ${APACHE_LOG_DIR}/error.log 
+    CustomLog ${APACHE_LOG_DIR}/access.log combined 
 </VirtualHost> 
-EOF 
